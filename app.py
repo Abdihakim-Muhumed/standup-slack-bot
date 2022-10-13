173b7975a12d2e2de25a3a9a21169d3ada7e033a
Abdihakim Muhumed <64124679+Abdihakim-Muhumed@users.noreply.github.com>
2022-10-13 14:06:05 +0300

    Add introductory message to home tab

--- app.py
+++ app.py
@@ -10,6 +10,36 @@ app = App(
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
+@app.event("app_home_opened")
+def update_home_tab_with_introduction(client,event,logger):
+    try:
+        client.views_publish(
+            user_id=event["user"],
+            view={
+                "type": "home",
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "*Welcome to _Stand up Bot_* :tada:"
+                        }
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "Hi, my name is Stand up Bot. I am a friendly bot and i will be in charge of the daily stand ups in this workspace."
+                        }
+                    }
+                ]
+            }
+        )
+    except Exception as e:
+            logger.error(f"Error publishing home tab introduction: {e}")
 
 
 if __name__ == "__main__":
