dd3aab414777fdae9e7af17c8ed0884bf78f96e7
Abdihakim Muhumed <64124679+Abdihakim-Muhumed@users.noreply.github.com>
2022-10-14 12:00:11 +0300

    feature- alert before sending standups

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+from http import client
 from lib2to3.pgen2 import token
 import os
+from sched import scheduler
 from webbrowser import get
 
 from slack_bolt import App
+from slack_sdk.errors import SlackApiError
 
+import schedule
+from schedule import every, repeat, run_pending
+import time
 
 app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
@@ -41,6 +47,24 @@ def update_home_tab_with_introduction(client,event,logger):
     except Exception as e:
             logger.error(f"Error publishing home tab introduction: {e}")
 
+@repeat(every().monday.at("18:00"))
+@repeat(every().tuesday.at("18:00"))
+@repeat(every().wednesday.at("18:00"))
+@repeat(every().thursday("18:00"))
+def alert_before_sending_standups(client=app.client):   
+    try:
+        result = client.chat_postMessage(
+            channel="#building-slack-bots",
+            text="Good evening everyone! It is Stand up time, I am sending out standups in 5 minutes time. "
+        )
+        return(result)
 
+    except SlackApiError as e:
+        return(f"Error: {e}")
+
+while app:
+    schedule.run_pending()
+    time.sleep(1)
+    
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
